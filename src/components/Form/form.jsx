f983dbfaf9a58e1da7fd082e4512db04f35f3320
Anna-K-S <anna.sergeevna1622@gmail.com>
2023-02-21 02:07:50 +0300

    init

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -4,8 +4,22 @@ import './form.css';
 const Form = () => {
 
 return (
-  <div>
-    Form
+  <div className={"form"}>
+    <h3>Введите ваши данные</h3>
+    <input
+     className={"input"} 
+     type="text"
+     placeholder={'Страна'}
+     />
+    <input
+      className={"input"}
+      type="text"
+      placeholder={'Улица'}
+     />
+     <select className={"select"}>
+      <option value={'legal'}>Юр.лицо</option>
+      <option value={'physical'}>Физ.лицо</option>
+     </select>
   </div>
 );
 
