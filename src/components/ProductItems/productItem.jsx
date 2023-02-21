85840d7194c6abc733ac4eac4d3270778b56aae2
Anna-K-S <anna.sergeevna1622@gmail.com>
2023-02-22 01:48:46 +0300

    init

--- src/components/ProductItems/productItem.jsx
+++ src/components/ProductItems/productItem.jsx
@@ -3,15 +3,15 @@ import './productItem.css';
 import Button from "../Button/Button";
 
 
-const ProductItem = (product, className, onAdd) => {
+const ProductItem = ({product, className, onAdd}) => {
   const onAddHandler = () => {
     onAdd(product);
   }
-  
+
   return (
     <div className={'product' + className}>
       <div className={'img'}/>
-      <div className={'title'}/>
+      <div className={'title'}>{product.title}</div>
       <div className={'description'}>{product.description}</div>
       <div className={'price'}>
         <span>Стоимость: <b>{product.price}</b></span>
