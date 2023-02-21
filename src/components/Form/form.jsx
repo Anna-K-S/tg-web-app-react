c1031e37154fa59bc78664c86e06983785bb5a36
Anna-K-S <anna.sergeevna1622@gmail.com>
2023-02-21 03:20:11 +0300

    init

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from 'react';
 import "./form.css";
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Form = () => {
+
+   const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
+
+    const onSendData = useCallback(() => {
+      const data = {
+          country,
+          street,
+          subject,
+      }
+      tg.sendData(JSON.stringify(data));
+  }, [country, street, subject])
+
+  useEffect(() => {
+      tg.onEvent('mainButtonClicked', onSendData)
+      return () => {
+          tg.offEvent('mainButtonClicked', onSendData)
+      }
+  }, [onSendData])
+
+  useEffect(() => {
+      tg.MainButton.setParams({
+          text: 'Отправить данные'
+      })
+  }, [])
+
+  useEffect(() => {
+      if(!street || !country) {
+          tg.MainButton.hide();
+      } else {
+          tg.MainButton.show();
+      }
+  }, [country, street])
+
+  const onChangeCountry = (e) => {
+      setCountry(e.target.value)
+  }
+
+  const onChangeStreet = (e) => {
+      setStreet(e.target.value)
+  }
+
+  const onChangeSubject = (e) => {
+      setSubject(e.target.value)
+  }
+
 
 return (
   <div className="form">
@@ -10,14 +59,19 @@ return (
      className="input" 
      type="text"
      placeholder={'Страна'}
+     value={country}
+     onChange={onChangeCountry}
      />
     <input
       className="input"
       type="text"
       placeholder={'Улица'}
+      value={street}
+      onChange={onChangeStreet}
      />
 
-     <select className={"select"}>
+     <select value={subject}
+     onChange={onChangeSubject} className={"select"}>
       <option value={"legal"}>Юр.лицо</option>
       <option value={"pysical"}>Физ.лицо</option>
      </select>
